07bcb0504496b2ffac96f1d5dbc4247e8ed9cc9a
John Doe <johndoe@example.com>
2015-07-01 11:45:48 +0800

    dd

---
+++ dsda/dsda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dsda
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dsda/dsda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dsda
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
